7d51b13f6ae1791470bc4a52b2092f3f88111f3e
Emacost96 <costantiniemanuele96@gmail.com>
2023-05-22 12:43:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projectwork-007-topsecret
+ 
---
+++ src/main/java/it/corso/Projectwork007TopsecretApplication.java
@@ -0,0 +1,13 @@
+package it.corso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectwork007TopsecretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projectwork007TopsecretApplication.class, args);
+	}
+
+}
