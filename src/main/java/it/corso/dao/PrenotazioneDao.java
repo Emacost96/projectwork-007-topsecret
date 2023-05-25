da3ee87e322105dd7ebcec969f93ad76ee3f79f6
Emacost96 <costantiniemanuele96@gmail.com>
2023-05-25 09:56:12 +0200

    Eliminato metodo getPenotazioniByIdUtente

--- src/main/java/it/corso/dao/PrenotazioneDao.java
+++ src/main/java/it/corso/dao/PrenotazioneDao.java
@@ -1,12 +1,10 @@
 package it.corso.dao;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 import it.corso.model.Prenotazione;
 
 public interface PrenotazioneDao extends CrudRepository<Prenotazione, Integer>{
 
-    List<Prenotazione> getPrenotazioniByIdUtente(int id);
+
 }
