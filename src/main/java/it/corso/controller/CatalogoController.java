8a32ff64dcbc98f51faacb0f4fcd75e4227c8d68
hnhsnt <hannahgssantos@gmail.com>
2023-05-26 17:34:39 +0200

    connessione catalogo back e front end

--- src/main/java/it/corso/controller/CatalogoController.java
+++ src/main/java/it/corso/controller/CatalogoController.java
@@ -12,28 +12,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import it.corso.model.Opera;
 import it.corso.service.OperaService;
 
-
-//questo genera la lista delle opere
 @Controller
-@RequestMapping("catalogo/film")
-public class FilmController {
-
+@RequestMapping("/catalogo")
+public class CatalogoController {
+	
 	@Autowired
 	private OperaService operaService;
 	
 	@GetMapping
-	public String getPage(Model model)
+	public String getPage(
+			Model model,
+			@RequestParam(name = "film", required = false) String f,
+			@RequestParam(name = "libri", required = false) String l) 
 	{
+		List<Opera> libri = operaService.getLibri();
+		model.addAttribute("libri", libri);
 		List<Opera> film = operaService.getFilm();
 		model.addAttribute("film", film);
-		return "film";
-	}
-	
-	
-	@GetMapping("elimina")
-	public String eliminaOpera(
-			@RequestParam("id") int id) {
-		operaService.eliminaOpera(operaService.findOperaById(id));
-		return "redirect:/=film";
+		model.addAttribute("f", f!=null);
+		model.addAttribute("l", l!=null);
+		return "catalogo";
 	}
 }
