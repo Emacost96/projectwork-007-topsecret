a6521f81817b2328430a141b922ae940b77ecc28
Emacost96 <costantiniemanuele96@gmail.com>
2023-05-29 15:54:36 +0200

    Aggiunto Thymleaf alla dashboard

--- src/main/java/it/corso/controller/AdminController.java
+++ src/main/java/it/corso/controller/AdminController.java
@@ -1,5 +1,6 @@
 package it.corso.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.corso.model.Opera;
+import it.corso.model.Utente;
 import it.corso.service.OperaService;
+import it.corso.service.UtenteService;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -20,13 +23,23 @@ public class AdminController {
 	
 	@Autowired
 	private OperaService operaService;
+	@Autowired
+	private UtenteService utenteService;
 	
 	@GetMapping
 	public String getPage(Model model, HttpSession session) {
 		if (session.getAttribute("admin") == null)
 			return "redirect:/utente";
+		List<Utente> utenti = utenteService.getUtenti();
 		List<Opera> opere =  operaService.getOpere();
+		List<Opera> operePrenotate = new ArrayList<>();
+		for (Opera o : opere) {
+		    if (o.isPrenotato() == true)
+			operePrenotate.add(o);
+		}
+		model.addAttribute("utenti", utenti);
 		model.addAttribute("opere", opere);
+		model.addAttribute("operePrenotate", operePrenotate);
 		return "dashboardAdmin";
 	}
 	
