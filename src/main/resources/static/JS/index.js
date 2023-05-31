02d9eb278ed57ad0e71f81a9adf46d672341d03d
Emacost96 <costantiniemanuele96@gmail.com>
2023-05-31 14:56:10 +0200

    aggiornamenti

--- src/main/resources/static/JS/index.js
+++ src/main/resources/static/JS/index.js
@@ -3,7 +3,9 @@ const emailNewsLetter = document.getElementById("email-newsletter")
 const confermaNewsLetter = document.getElementById("conferma-newsletter")
 
 btnNewsLetter.addEventListener("click", function(){
-	btnNewsLetter.style.display = "none"
-	emailNewsLetter.style.display = "none"
-	confermaNewsLetter.style.display = "block";
+	if(emailNewsLetter.value == " "){
+		btnNewsLetter.style.display = "none"
+		emailNewsLetter.style.display = "none"
+		confermaNewsLetter.style.display = "block";
+	}
 })
